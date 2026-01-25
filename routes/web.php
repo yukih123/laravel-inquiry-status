8f33e799777e30c0469fa486cdc52d40c62420ee
yukih <hiromi.yuki1@gmail.com>
2026-01-26 01:04:19 +0900

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InquiryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inquiry', [InquiryController::class, 'create']);
+Route::post('/inquiry', [InquiryController::class, 'store']);
+
+Route::get('/admin/inquiries', [InquiryController::class, 'index']);
+
