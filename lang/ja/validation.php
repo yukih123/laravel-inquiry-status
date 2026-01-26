9a7729e93cd623d5c24bf36a6128c529cf232782
yukih <hiromi.yuki1@gmail.com>
2026-01-26 19:43:30 +0900

    Add japanese validation, change locale

---
+++ lang/ja/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'required' => ':attribute は必須項目です。',
+    'email'    => ':attribute の形式が正しくありません。',
+    'max' => [
+        'string' => ':attribute は :max 文字以内で入力してください。',
+    ],
+
+    'attributes' => [
+        'name'    => 'お名前',
+        'email'   => 'メールアドレス',
+        'message' => 'お問い合わせ内容',
+    ],
+];
+
