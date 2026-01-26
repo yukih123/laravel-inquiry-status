cc239cf0146882689ebf79989275d0d631cfd12e
yukih <hiromi.yuki1@gmail.com>
2026-01-26 19:35:11 +0900

    Add layout, css

--- resources/views/inquiry/create.blade.php
+++ resources/views/inquiry/create.blade.php
@@ -5,36 +5,46 @@
     <title>お問い合わせ</title>
 </head>
 <body>
-<h1>お問い合わせ</h1>
 
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+@extends('layouts.app')
 
-<form method="POST" action="{{ route('inquiry.store') }}">
-    @csrf
+@section('title', 'お問い合わせ')
 
-    <div>
-        名前<br>
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
+@section('content')
+    <h1>お問い合わせ</h1>
 
-    <div>
-        メール<br>
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+    @if ($errors->any())
+        <p class="error">入力内容にエラーがあります。</p>
+    @endif
 
-    <div>
-        内容<br>
-        <textarea name="message">{{ old('message') }}</textarea>
-    </div>
+    <form method="POST" action="{{ route('inquiry.store') }}">
+        @csrf
 
-    <button type="submit">送信</button>
-</form>
-</body>
-</html>
+        @error('name')
+            <p class="error">{{ $message }}</p>
+        @enderror
+        <div>
+            名前<br>
+            <input type="text" name="name" class="{{ $errors->has('name') ? 'input-error' : '' }}" value="{{ old('name') }}">
+        </div>
 
+        @error('email')
+            <p class="error">{{ $message }}</p>
+        @enderror
+        <div>
+            メール<br>
+            <input type="email" name="email" class="{{ $errors->has('email') ? 'input-error' : '' }}" value="{{ old('email') }}">
+        </div>
+
+        @error('message')
+            <p class="error">{{ $message }}</p>
+        @enderror
+        <div>
+            内容<br>
+            <textarea name="message" class="{{ $errors->has('message') ? 'input-error' : '' }}">{{ old('message') }}</textarea>
+        </div>
+
+        <button type="submit">送信</button>
+    </form>
+
+@endsection
